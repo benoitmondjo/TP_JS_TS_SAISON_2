4415b8388f76a27f872200415f1b3e1eca95ef5e
benoitmondjo <benoitmondjo@gmail.com>
2026-05-06 12:53:15 +0100

    episode-2. Update episode-1

--- js/app.js
+++ js/app.js
@@ -1,11 +1,34 @@
-import {isValidEmail} from './fonctions';
+import {isValidEmail, isValidePassword} from './fonctions';
+
+/*
 
 let email = prompt("Entrez votre adresse e-mail :");
 
+if(email !== null){
 
-if(isValidEmail(email)){
-    alert(`Email valide`)
+    if(isValidEmail(email)){
+        alert(`Email valide`)
+    }
+
+    else{
+        alert(`Votre email est invalide.`)
+    }
 }
-else{
-    alert(`Votre email est invalide.`)
+
+*/
+
+let password = prompt("Entrer un mot de passe.");
+
+
+if(password !== null){
+
+    const res = isValidePassword(password);
+    
+    if(typeof(res)=== "boolean"){
+        alert("mot de passe validé.")
+    }
+    else{
+        alert(res)
+    }
+
 }
--- js/fonctions.js
+++ js/fonctions.js
@@ -15,4 +15,36 @@ function isValidEmail(email){
     }
 }
 
-export {isValidEmail}
+
+
+/**
+ * Fonction permetant de valide un mot de passe
+ * @param {string} password - mot de passe saisis par le user
+ * @returns {boolean | Array} - True si password valié. Sinon un tableau contenant les messages d'erreurs. 
+ */
+function isValidePassword(password){
+
+    const regexMaj = /[A-Z]/;
+    const regexNumber = /[0-9]/
+    
+    let errors = Array();
+
+    if(password.length < 8){
+        errors.push("Le mot de passe doit contenir au moins 8 caracters.")
+    }
+    else if(!regexMaj.test(password)){
+        errors.push("Le mot de passe doit contenir au moins Une lettre majuscule.")
+    }
+    else if(!regexNumber.test(password)){
+        errors.push("Le mot de passe doit contenir au moins un nombre.")
+    }
+
+    if(errors.length === 0){
+        return true;
+    }
+    else{
+        return errors;
+    }
+}
+
+export {isValidEmail, isValidePassword}
